7250ededf81b7104b1d7d548a8d26921e9c153b1
Danny Chen <37634165+zhuodannychen@users.noreply.github.com>
2020-03-28 15:15:22 -0500

    Update Trie.cpp

--- algorithms/graphs/Trie.cpp
+++ algorithms/graphs/Trie.cpp
@@ -3,40 +3,40 @@ using namespace std;
 
 const int MaxNodes = 200005;
 const int alphabet = 26;
-int trie[MaxNodes][alphabet], cnt[MaxNodes];
-int sz = 1;
+int sz, trie[MaxNodes][alphabet], cnt[MaxNodes];
 
-struct Trie {
-    void insert(string str) {
-        int u = 0;
-        for (char c : str) {
-            if (!trie[u][c-'A'])
-                trie[u][c-'A'] = sz++;
-            u = trie[u][c-'A'];
-        }
-        ++cnt[u];
+void insert(string str) {
+    int u = 0;
+    for (char c : str) {
+        if (!trie[u][c-'A'])
+            trie[u][c-'A'] = sz++;
+        u = trie[u][c-'A'];
     }
+    ++cnt[u];
+}
 
-    bool search (string str) {
-        int u = 0;
-        for (char c : str) {
-            if (!trie[u][c-'A'])
-                return false;
-            u = trie[u][c-'A'];
-        }
-        return cnt[u] > 0;
+bool search (string str) {
+    int u = 0;
+    for (char c : str) {
+        if (!trie[u][c-'A'])
+            return false;
+        u = trie[u][c-'A'];
     }
-};
+    return cnt[u] > 0;
+}
 
 int main() {
-    Trie trie;
+    memset(trie, 0, sizeof(trie[0])*sz);
+	memset(cnt, 0, 4*sz);
+    sz = 1;
+
     string strs[] = {"ALICE", "BOB", "CHARLIE", "DANNY", "ELSIE"};
     string output[] = {"NO", "YES"};
 
     for (int i = 0; i < 5; i++)
-        trie.insert(strs[i]);
+        insert(strs[i]);
 
-    cout << output[trie.search("BOB")] << "\n";
-    cout << output[trie.search("DANIEL")] << "\n";
-    cout << output[trie.search("DANN")] << "\n";
+    cout << output[search("BOB")] << "\n";
+    cout << output[search("DANIEL")] << "\n";
+    cout << output[search("DANN")] << "\n";
 }
