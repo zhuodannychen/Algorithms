6af1cae9a9c048229194948a9cd453a940672221
Danny Chen <37634165+zhuodannychen@users.noreply.github.com>
2020-05-21 16:10:50 -0500

    Create floyd_warshall.cpp

---
+++ algorithms/graphs/floyd_warshall.cpp
@@ -0,0 +1,45 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define INF 1000000010
+
+int n, dist[305][305];
+
+void floyd_warshall() {
+    for (int k = 0; k < n; k++) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (dist[i][k] < INF && dist[k][j] < INF)
+                    dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j]);
+            }
+        }
+    }
+}
+
+int main() {
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            cin >> dist[i][j];
+        }
+    }
+
+    floyd_warshall();
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            cout << dist[i][j] << " ";
+        }
+        cout << "\n";
+    }
+}
+
+/*
+Test Case:
+4
+0 5 10000 10
+10000 0 3 10000
+10000 10000 0 1
+10000 10000 10000 0
+
+*/
